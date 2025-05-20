bb4acd5f7b3a24ea506f06c453ea4ca0249d05b5
Gabriel Lima S Santos <86032259+GLima2021@users.noreply.github.com>
2025-05-20 11:17:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
 // Arquivo: conexao.php
 
-$host = 'localhost';      // Endereço do servidor MySQL
-$usuario = 'tayantad_leads';        // Nome de usuário do MySQL
-$senha = 'Pinexpress123';              // Senha do MySQL
-$banco = 'tayantad_leads'; // Nome do banco de dados
+$host = 'HOST';      // Endereço do servidor MySQL
+$usuario = 'USUARIO';        // Nome de usuário do MySQL
+$senha = 'SENHA;              // Senha do MySQL
+$banco = 'BANCO DE DADOS'; // Nome do banco de dados
 
 // Criar conexão
 $conexao = new mysqli($host, $usuario, $senha, $banco);
